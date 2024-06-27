d7cebaadb193b17fe4789cca53ae6ba38bbd7c9b
Peter-Mwangi254 <pm6093628@gmail.com>
2024-06-27 12:18:41 -0700

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    a function that determines if a given data set represens a valid
+    UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+     
+    """Determines if given data represents valid UTF-8 encoding
+    Args:
+        data: list of integers
+    Returns:
+        True if valid UTF-8 encoding, otherwise False
+    """
+    n_bytes = 0
+    for num in data:
+        if n_bytes == 0:
+            if (num >> 7) == 0b0:
+                continue
+            elif (num >> 5) == 0b110:
+                n_bytes = 1
+            elif (num >> 4) == 0b1110:
+                n_bytes = 2
+            elif (num >> 3) == 0b11110:
+                n_bytes = 3
+            else:
+                return False
+        else:
+            if (num >> 6) != 0b10:
+                return False
+            n_bytes -= 1
+    return n_bytes == 0
