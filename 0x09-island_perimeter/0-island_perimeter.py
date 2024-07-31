d974decdb4a607005d154acfa4ef08009b73484d
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-31 04:33:30 -0700

    pycodestyle changes

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,19 +10,14 @@ def island_perimeter(grid):
     """
     if not grid:
         return 0
-    
     perimeter = 0
     rows, cols = len(grid), len(grid[0])
-    
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
-                
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
-    
     return perimeter
